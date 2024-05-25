e5cfa049159bfb7ab89a7dece7c81962c53f9ebe
madaooftheblues <azlanrasool@gmail.com>
2024-05-26 03:58:55 +0500

    Add function bfs and knightMoves

--- knights-travails.js
+++ knights-travails.js
@@ -22,4 +22,52 @@ function isOutOfBounds(p) {
     return p.x > MATRIX_SIZE - 1 || p.y > MATRIX_SIZE - 1 || p.x < 0 || p.y < 0
 }
 
+function isEqual(a, b) {
+    return a.x === b.x && a.y === b.y
+}
+
+function bfs(start, target) {
+    const frontier = [start]
+
+    while (frontier.length) {
+        const current = frontier.pop(0)
+        matrix[current.x][current.y] = 1
+
+        console.log(frontier)
+
+        if (isEqual(current, target)) return true
+
+        const i = current.x
+        const j = current.y
+
+        const possiblePoses = [
+            new Position(i - 2, j - 1),
+            new Position(i - 2, j + 1),
+            new Position(i - 1, j - 2),
+            new Position(i - 1, j + 2),
+            new Position(i + 2, j - 1),
+            new Position(i + 2, j + 1),
+            new Position(i + 2, j + 1),
+            new Position(i + 1, j - 1),
+            new Position(i + 1, j + 1),
+        ]
+
+        for (p of possiblePoses)
+            if (!isOutOfBounds(p) && !matrix[p.x][p.y]) frontier.push(p)
+    }
+}
+
 const matrix = createMatrix()
+
+function knightMoves(current, target) {
+    if (isOutOfBounds(current) || isOutOfBounds(target)) {
+        console.log('Invalid positions specified')
+        return
+    }
+
+    const result = bfs(current, target)
+
+    return result
+}
+
+console.log(knightMoves({ x: 2, y: 3 }, { x: 2, y: 6 }))
